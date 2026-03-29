a72e5d856eb28469960d53a7ee4136c1aee09834
gusmccoy <gusmccoycollege@gmail.com>
2026-03-29 15:53:35 -0500

    Initial Commit

---
+++ caffeinated-avocados/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  caffeinated-avocados
+//
+//  Created by Gus McCoy on 3/29/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ caffeinated-avocadosTests/caffeinated_avocadosTests.swift
@@ -0,0 +1,18 @@
+//
+//  caffeinated_avocadosTests.swift
+//  caffeinated-avocadosTests
+//
+//  Created by Gus McCoy on 3/29/26.
+//
+
+import Testing
+
+struct caffeinated_avocadosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
